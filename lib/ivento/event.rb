ac0fe37c8e7610671a4a9803300210b92220a722
Anton Davydov <antondavydov.o@gmail.com>
2019-04-06 14:14:21 +0300

    Rewrite event module

--- lib/ivento/event.rb
+++ lib/ivento/event.rb
@@ -7,10 +7,10 @@ module Ivento
         include Dry::Types.module
       end
 
-      attribute :eid, Types::String.default { SecureRandom.uuid }
+      attribute :eid,        Types::String.default { SecureRandom.uuid }
       attribute :created_at, Types::Time.default { Time.now }
-      attribute :version, Types::String.default('v1').optional
-      attribute :stream, Types::String.default('')
+      attribute :version,    Types::String.default('v1')
+      attribute :stream,     Types::String.default('')
 
       def self.payload_attributes(value = nil)
         if value
@@ -23,7 +23,7 @@ module Ivento
       attribute :payload, payload_attributes
 
       def inspect
-        "#{self.class.name} (#{version}) (#{eid}) payload: #{data.inspect}"
+        "#{self.class.name} (#{version}) (#{eid}) payload: #{payload.inspect}"
       end
     end
   end
